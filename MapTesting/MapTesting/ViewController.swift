b1e1e8c8d760e82114aadd5e993af0aeaf0ae1ad
Mauricio Chirino <mchirino89@icloud.com>
2020-10-31 20:48:19 -0300

    Setup dependencies

---
+++ MapTesting/MapTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapTesting
+//
+//  Created by Mauricio Chirino on 31/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
