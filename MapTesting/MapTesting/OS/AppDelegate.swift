0561371f523d9d16b00bed11d54b5ca40012a586
Mauricio Chirino <1657723+mchirino89@users.noreply.github.com>
2020-11-02 12:10:51 -0300

    Update AppDelegate.swift
    Remove google map key

--- MapTesting/MapTesting/OS/AppDelegate.swift
+++ MapTesting/MapTesting/OS/AppDelegate.swift
@@ -10,7 +10,7 @@ import GoogleMaps
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        GMSServices.provideAPIKey("AIzaSyDj85vTmuHnPcP99JdZEHGHmSVoq817kfU")
+        GMSServices.provideAPIKey("")
         
         return true
     }
